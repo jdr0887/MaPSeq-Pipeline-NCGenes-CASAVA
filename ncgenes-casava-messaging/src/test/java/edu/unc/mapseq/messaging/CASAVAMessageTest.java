6861f9f4cb3b4beead4c76b0d2ad7753ada1c9b8
Jason Reilly <jdr0887@gmail.com>
2016-08-02 12:43:52 -0400

    posterity commit

--- ncgenes-casava-messaging/src/test/java/edu/unc/mapseq/messaging/CASAVAMessageTest.java
+++ ncgenes-casava-messaging/src/test/java/edu/unc/mapseq/messaging/CASAVAMessageTest.java
@@ -45,6 +45,20 @@ public class CASAVAMessageTest {
             generator.writeStartObject();
             generator.writeStringField("entityType", "WorkflowRun");
             generator.writeStringField("name", "NCG.160601_UNC18-D00493_0325_BC8GP3ANXX.CASAVA");
+            
+            generator.writeArrayFieldStart("attributes");
+
+            generator.writeStartObject();
+            generator.writeStringField("name", "allowMismatches");
+            generator.writeStringField("value", "true");
+            generator.writeEndObject();
+
+            generator.writeStartObject();
+            generator.writeStringField("name", "barcodeLength");
+            generator.writeStringField("value", "6");
+            generator.writeEndObject();
+            
+            generator.writeEndArray();
             generator.writeEndObject();
 
             generator.writeEndArray();
