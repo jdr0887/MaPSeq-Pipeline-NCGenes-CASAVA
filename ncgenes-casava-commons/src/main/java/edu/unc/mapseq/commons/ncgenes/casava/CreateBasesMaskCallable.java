a699241d26eb600f2e899fa8aa1ba706efd20cba
Jason Reilly <jdr0887@gmail.com>
2018-01-11 08:58:21 -0500

    wrong column

--- ncgenes-casava-commons/src/main/java/edu/unc/mapseq/commons/ncgenes/casava/CreateBasesMaskCallable.java
+++ ncgenes-casava-commons/src/main/java/edu/unc/mapseq/commons/ncgenes/casava/CreateBasesMaskCallable.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 
